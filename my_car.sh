e857878e2902ff592d314d3f274b390defb3f86c
captinsteven <captinsteven@163.com>
2018-12-31 15:18:34 +0800

    add distributed and two new files--'my_car.sh and wifi_car.py'

---
+++ my_car.sh
@@ -0,0 +1,2 @@
+python /home/albert/CarProgram/wifi_car.py & cd /home/albert/mjpg-streamer/mjpg-streamer-experimental
+./mjpg_streamer -i "./input_raspicam.so" -o "./output_http.so -p 8090 -w ./www"
